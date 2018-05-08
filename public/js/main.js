e7910d05802ca5b16922a0e085bd0a980ec137bc
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-05-08 15:53:26 -0400

    Move JS to own file
    I did some other things here. You tried to load firebase twice, called some functions in weird places (I moved them to more sensible places), etc.

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+var signedIn;
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    signedIn = true;
+    // User is signed in.
+  } else {
+    signedIn = false;
+    // No user is signed in.
+  }
+  setButtons(signedIn);
+});
+
+function gLogin() {
+  firebase.auth().signInWithPopup(gProvider).then(function (result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    signedIn = true; // you were running this in the "catch" block, which is called when an error occurs. 
+    setButtons(signedIn);
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function gLogout() {
+  firebase.auth().signOut().then(function () {
+    signedIn = false;
+    setButtons(signedIn);
+  }).catch(function (error) {
+    // An error happened.
+  });
+}
+
+function setButtons(loggedIn) {
+  if (loggedIn) {
+    document.getElementById("signin").setAttribute("style", "display: none;");
+    document.getElementById("signout").setAttribute("style", "display: block;");
+  } else {
+    document.getElementById("signin").setAttribute("style", "display: block;");
+    document.getElementById("signout").setAttribute("style", "display: none;");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
+});
